89c6aaeee1ab771743298e2d7d4da7ccf851e8c2
Arber Zela <zelaa@cs.uni-freiburg.de>
2020-04-22 21:22:21 +0200

    updated cluster scripts

--- cluster_scripts/bohb_one_shot_scripts/bohb-darts.sh
+++ cluster_scripts/bohb_one_shot_scripts/bohb-darts.sh
@@ -14,4 +14,4 @@
 #
 #
 source activate tensorflow-stable
-python optimizers/bohb_one_shot/master.py --array_id $SLURM_ARRAY_TASK_ID --total_num_workers 16 --num_iterations 64 --run_id $SLURM_ARRAY_JOB_ID --working_directory ./experiments/bohb_logs/bohb_output/ --min_budget 25 --max_budget 100 --seed 1 --space $1 --algorithm $2 --cs $3
+python optimizers/bohb_one_shot/master.py --array_id $SLURM_ARRAY_TASK_ID --total_num_workers 16 --num_iterations 64 --run_id $SLURM_ARRAY_JOB_ID --working_directory ./experiments/bohb_output/cs$3 --min_budget 25 --max_budget 100 --space $1 --algorithm $2 --cs $3 --seed $4
--- cluster_scripts/bohb_one_shot_scripts/start_exp.sh
+++ cluster_scripts/bohb_one_shot_scripts/start_exp.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-spaces='1 2'
+spaces='1 2 3'
 algs='darts gdas pc_darts'
+cs='1 2 3'
+seed='1 2 3'
 
 for s in $spaces; do
        for a in $algs; do
-	       sbatch scripts/bohb-darts.sh $s $a
-	       echo submitted job $s $a
+	       for c in $cs; do
+		       for sd in $seed; do
+			       sbatch -J ${s}_${c}_${sd}_${a} cluster_scripts/bohb_one_shot_scripts/bohb-darts.sh $s $a $c $sd
+			       echo submitted job: space $s, $a, cs $c, seed $sd
+		       done
+	       done
        done
-done       
+done
